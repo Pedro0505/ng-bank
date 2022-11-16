5d5d388bac4e34d7d45574bd5934cf26f6255448
pedro <pedrohrds05@hotmail.com>
2022-11-16 10:40:22 -0300

    refactor: add a next param in iusercontroller

--- api/src/entities/users/interfaces/IUsersController.ts
+++ api/src/entities/users/interfaces/IUsersController.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 interface IUserController {
-  login(req: Request, res: Response): Promise<Response>
-  register(req: Request, res: Response): Promise<Response>
+  login(req: Request, res: Response, next?: NextFunction): Promise<Response>
+  register(req: Request, res: Response, next?: NextFunction): Promise<Response>
 }
 
 export default IUserController;
