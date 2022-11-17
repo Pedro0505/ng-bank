8a0e64587a4eb84ede8bb8030f8d455e1e5c66f5
pedro <pedrohrds05@hotmail.com>
2022-11-17 10:19:37 -0300

    feat: add enum to get routes in tests

---
+++ api/__tests__/constants/PathRoutes.ts
@@ -0,0 +1,10 @@
+enum PathRoutes {
+  userLogin = '/user/login',
+  userRegister = '/user/register',
+  accountsGetBalanceById = '/accounts',
+  accountsCashOut = '/accounts/cashOut',
+  transactionGetByAccountId = '/transactions',
+  transactionFilters = '/transactions/filter',
+}
+
+export default PathRoutes;
