9a12357fc409be8c2f8b313cf7af08c61c2626d0
pedro <pedrohrds05@hotmail.com>
2022-11-17 10:19:20 -0300

    feat: add db seeds to tests

---
+++ api/__tests__/seeders/transactions.ts
@@ -0,0 +1,39 @@
+const transactions = [
+  {
+    id: '3174b700-64c7-42d3-8e5c-959f5c28b040',
+    debitedAccountId: 'd4258e37-e297-45f1-9bcd-487a65f93bfb',
+    creditedAccountId: 'fd06d274-75ba-475e-91db-813876fe58fa',
+    value: 20.00,
+    createdAt: new Date('2022-11-17')
+  },
+  {
+    id: 'fb5dfe6c-79c3-4483-9765-a03944e857ea',
+    debitedAccountId: 'fd06d274-75ba-475e-91db-813876fe58fa',
+    creditedAccountId: '55b47bb1-fe33-41eb-ae67-50b0d3f7229e',
+    value: 20.00,
+    createdAt: new Date('2022-11-18')
+  },
+  {
+    id: '62c132bd-d136-4687-9f3d-d194ff07c397',
+    debitedAccountId: '55b47bb1-fe33-41eb-ae67-50b0d3f7229e',
+    creditedAccountId: '7b796238-49d2-45e0-bcbe-6c9b2c924b2d',
+    value: 20.00,
+    createdAt: new Date('2022-11-19')
+  },
+  {
+    id: 'f9126e5a-3a8b-49f7-9670-3931cb74392d',
+    debitedAccountId: '7b796238-49d2-45e0-bcbe-6c9b2c924b2d',
+    creditedAccountId: '3eaa0cc0-05bb-4ce0-a1b4-4ab6cb4a52cb',
+    value: 20.00,
+    createdAt: new Date('2022-11-19')
+  },
+  {
+    id: '247cf092-f6a9-4cbf-8c46-ab568d8ecbc1',
+    debitedAccountId: '3eaa0cc0-05bb-4ce0-a1b4-4ab6cb4a52cb',
+    creditedAccountId: 'd4258e37-e297-45f1-9bcd-487a65f93bfb',
+    value: 20.00,
+    createdAt: new Date('2022-11-23')
+  },
+];
+
+export default transactions;
---
+++ api/__tests__/seeders/users.ts
@@ -0,0 +1,34 @@
+const users = [
+  {
+    id: 'c7cf3be0-bebf-4556-9127-a12f4caecebd',
+    username: 'jonh_doe',
+    password: '$2a$12$d7VvMETNivPPOUES7WUf3eWa42.AtqrObFhq.XQD/a88iT/OnGV/G', // 12345678A
+    accountId: 'd4258e37-e297-45f1-9bcd-487a65f93bfb',
+  },
+  {
+    id: 'a26d8353-f3f6-428f-95c1-657cd342b24b',
+    username: 'pedro_henrique',
+    password: '$2a$12$1twOHD6s/eVp2mGf8.I7BORqMZ0gNaYIj47g/T9b5gMmbb.Paxx.K', // 12345678B
+    accountId: 'fd06d274-75ba-475e-91db-813876fe58fa',
+  },
+  {
+    id: 'b60146c5-c90a-4da3-9b33-e91412306116',
+    username: 'pedro_brito',
+    password: '$2a$12$SW3AzaQv3qc./FhmKcnwIuWYJZ1vgXZTboPeSAM3WrcqU1uSzdYjy', // 12345678C
+    accountId: '55b47bb1-fe33-41eb-ae67-50b0d3f7229e',
+  },
+  {
+    id: 'c58a2e05-96c3-4a58-af86-ed6e1112d685',
+    username: 'joana_maria',
+    password: '$2a$10$9lpkMM7o9xwfItCTZfdLIuWmnOcPNeMm/Uv0Bta5oJV62fOYyeu42', // 12345678D
+    accountId: '7b796238-49d2-45e0-bcbe-6c9b2c924b2d',
+  },
+  {
+    id: '766b8282-0c24-4ae7-957e-198db8aa52c3',
+    username: 'clara_ribeiro',
+    password: '$2a$10$qkURB2BFyT3PEGXz86rOfOjjyqUKIQxBPx1n3FDR8hO99cribfiZK', // 12345678E
+    accountId: '3eaa0cc0-05bb-4ce0-a1b4-4ab6cb4a52cb',
+  },
+];
+
+export default users;
