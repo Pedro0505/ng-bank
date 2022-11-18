167c2515b4e4c4c7094a4439914e9859c334b6e9
pedro <pedrohrds05@hotmail.com>
2022-11-18 18:00:44 -0300

    feat: add singup page

---
+++ web/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Login from '../../components/Login/Login';
+import Register from '../../components/Register/Register';
+
+function SignUp() {
+  return (
+    <main>
+      <Register />
+      <Login />
+    </main>
+  );
+}
+
+export default SignUp;
