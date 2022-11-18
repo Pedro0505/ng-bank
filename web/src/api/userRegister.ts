2373119819d5366a08d7906a60993ee3c48f2307
pedro <pedrohrds05@hotmail.com>
2022-11-18 18:00:06 -0300

    fix: fix the path in request

--- web/src/api/userRegister.ts
+++ web/src/api/userRegister.ts
@@ -3,7 +3,7 @@ import ICreateUserRegister from '../interfaces/ICreateUserRegister';
 import IRegisterResponse from '../interfaces/IRegisterResponse';
 
 const userRegister = async (user: ICreateUserRegister): Promise<IRegisterResponse> => {
-  const login = await myAxios.post('/user/login', {
+  const login = await myAxios.post('/user/register', {
     username: user.username,
     password: user.password,
   });
