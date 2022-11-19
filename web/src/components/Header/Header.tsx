802e6f7205bfaf681299aa796ab4d649defcc2cf
pedro <pedrohrds05@hotmail.com>
2022-11-18 21:40:02 -0300

    feat: add header component

---
+++ web/src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+import { Button } from '@mui/material';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
+import deleteUserToken from '../../helpers/deleteUserToken';
+
+function Header() {
+  const { fetchUser, userToken } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  const handleLogOut = () => {
+    deleteUserToken();
+
+    navigate('/');
+  };
+
+  return (
+    <header>
+      <h1>NG.BANK</h1>
+      <h1>Olá, {userToken.data.username}</h1>
+      <Button variant="contained" size="large" onClick={ handleLogOut }>Logout</Button>
+    </header>
+  );
+}
+
+export default Header;
