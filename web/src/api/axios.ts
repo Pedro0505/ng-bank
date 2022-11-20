f2f8c4938a990b24e8871b61749ecb21185581fb
pedro <pedrohrds05@hotmail.com>
2022-11-20 16:17:17 -0300

    build: add env to backend in frontend container

--- web/src/api/axios.ts
+++ web/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const myAxios = axios.create({
-  baseURL: process.env.REACT_APP_PUBLIC_API,
+  baseURL: process.env.REACT_APP_PUBLIC_API || 'http://localhost:3001',
 });
 
 export default myAxios;
